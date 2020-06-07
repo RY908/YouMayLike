2b836a833ba173eb50cbf3f8aabe106c12217e09
永井琉生 <ryusei@nagairyuseinoMacBook-puro.local>
2020-06-07 18:06:05 +0900

    urls.pyの修正

--- YML/urls.py
+++ YML/urls.py
@@ -5,8 +5,7 @@ from . import views
 app_name = 'YML'
 urlpatterns = [
     path('', views.index, name='index'),
-    # ex: /YML/song_name/results/
+    # ex: /YML/artist_name/results/
     path('<str:name>/results/', views.spotify, name='spotify'),
-    #path('result/', views.spotify, name='result'),
     path('form/', views.NameForm, name='form'),
 ]
